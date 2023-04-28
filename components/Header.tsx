20ab9503dd50d2b87036f53a69a035a5e72e71a2
yyx20202020 <126185233+yyx20202020@users.noreply.github.com>
2023-04-28 20:07:14 +0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
 
   return (
     <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
-      <Link href="/" className="flex space-x-3">
+      <Link href="https://www.gptplus.wiki/" className="flex space-x-3">
         <Image
           alt="header text"
           src="/icon.svg"
